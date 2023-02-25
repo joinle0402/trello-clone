f9836809b5ecb51ad8eb10df546d442344faf1ee
John Smith <nhoxhero073@gmail.com>
2023-02-25 09:40:32 +0700

    Add Feature 'Add New Column'

--- src/components/BoardColumn.js
+++ src/components/BoardColumn.js
@@ -14,30 +14,31 @@ function BoardColumn({ column, onColumnCardDrog }) {
             >
                 <h4 className="text-base font-bold">{column.name}</h4>
             </header>
-
-            <ul className="max-h-[520px] overflow-auto scrollbar  p-3">
-                <Container
-                    {...column.props}
-                    groupName="board-column"
-                    onDrop={(dropResult) => onColumnCardDrog(column.id, dropResult)}
-                    getChildPayload={(index) => cards[index]}
-                    dragClass="column-card-ghost"
-                    dropClass="column-card-ghost-drop"
-                    dragHandleSelector=".column-card-drag-handle"
-                    dropPlaceholder={{
-                        animationDuration: 150,
-                        showOnTop: true,
-                        className: "column-card-drop-preview",
-                    }}
-                    dropPlaceholderAnimationDuration={200}
-                >
-                    {cards.map((card, index) => (
-                        <Draggable key={card.id} className="mt-2 first:mt-0">
-                            <ColumnCard key={index} card={card} />
-                        </Draggable>
-                    ))}
-                </Container>
-            </ul>
+            {cards.length > 0 && (
+                <ul className="max-h-[520px] overflow-auto scrollbar p-3">
+                    <Container
+                        {...column.props}
+                        groupName="board-column"
+                        onDrop={(dropResult) => onColumnCardDrog(column.id, dropResult)}
+                        getChildPayload={(index) => cards[index]}
+                        dragClass="column-card-ghost"
+                        dropClass="column-card-ghost-drop"
+                        dragHandleSelector=".column-card-drag-handle"
+                        dropPlaceholder={{
+                            animationDuration: 150,
+                            showOnTop: true,
+                            className: "column-card-drop-preview",
+                        }}
+                        dropPlaceholderAnimationDuration={200}
+                    >
+                        {cards.map((card, index) => (
+                            <Draggable key={card.id} className="mt-2 first:mt-0">
+                                <ColumnCard key={index} card={card} />
+                            </Draggable>
+                        ))}
+                    </Container>
+                </ul>
+            )}
 
             <footer className="flex items-center h-10 p-3 border-t border-slate-300">
                 <button className="w-full px-4 py-1 text-base font-bold text-white bg-blue-700 rounded">
--- src/components/BoardContent.js
+++ src/components/BoardContent.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Container, Draggable } from "react-smooth-dnd";
 
 import BoardColumn from "components/BoardColumn";
@@ -8,6 +8,9 @@ import { applyDrag } from "utilities/applyDrag";
 
 function BoardContent() {
     const [columns, setColumns] = useState([]);
+    const [isOpenNewColumn, setIsOpenNewColumn] = useState(false);
+    const [newColumnTitle, setNewColumnTitle] = useState("");
+    const inputNewColumnRef = useRef();
 
     useEffect(() => {
         const foundBoard = database.boards.find((board) => board.id === "board-1");
@@ -15,6 +18,15 @@ function BoardContent() {
             setColumns(mapOrder(foundBoard.columns, foundBoard.columnOrder, "id"));
         }
     }, []);
+
+    useEffect(() => {
+        if (isOpenNewColumn && inputNewColumnRef.current) {
+            inputNewColumnRef.current.focus();
+            inputNewColumnRef.current.select();
+        }
+    }, [isOpenNewColumn]);
+
+    const toggleOpenNewColumn = () => setIsOpenNewColumn(!isOpenNewColumn);
 
     const onBoardColumnDrog = (dropResult) => {
         const copyColumns = [...columns];
@@ -32,8 +44,22 @@ function BoardContent() {
         }
     };
 
+    const createNewColumn = () => {
+        if (newColumnTitle) {
+            const newColumn = {
+                id: Date.now(),
+                name: newColumnTitle,
+                cards: [],
+                cardOrder: [],
+            };
+            setColumns([...columns, newColumn]);
+            setNewColumnTitle("");
+            setIsOpenNewColumn(false);
+        }
+    };
+
     return (
-        <div className="flex items-start gap-3 p-3 overflow-x-auto bg-blue-700 scrollbar overflow-y-none">
+        <div className="flex items-start gap-3 p-3 overflow-x-auto overflow-y-hidden bg-blue-700 scrollbar">
             <Container
                 orientation="horizontal"
                 onDrop={onBoardColumnDrog}
@@ -51,6 +77,61 @@ function BoardContent() {
                     </Draggable>
                 ))}
             </Container>
+            {!isOpenNewColumn ? (
+                <div
+                    className="bg-blue-600 hover:bg-blue-500 cursor-pointer text-white rounded w-80 min-w-[320px] max-h-full board-column h-10 flex items-center gap-1 px-3 ml-1"
+                    onClick={toggleOpenNewColumn}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                    >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    <span>Thêm danh sách khác</span>
+                </div>
+            ) : (
+                <div className="bg-blue-100 rounded w-80 min-w-[320px] max-h-full board-column ml-1" >
+                    <div className="px-2 py-1">
+                        <input
+                            type="text"
+                            id="first_name"
+                            className="block w-full p-2 text-sm text-gray-900 border border-gray-400 rounded bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                            placeholder="John"
+                            ref={inputNewColumnRef}
+                            value={newColumnTitle}
+                            onChange={(e) => setNewColumnTitle(e.target.value)}
+                            onKeyDown={(keyEvent) => keyEvent.key === "Enter" && createNewColumn()}
+                            required
+                        />
+                    </div>
+                    <div className="flex items-center gap-3 px-2 py-1">
+                        <button
+                            type="button"
+                            className="px-2 py-2 text-sm font-medium text-white bg-blue-700 rounded hover:bg-blue-800"
+                            onClick={createNewColumn}
+                        >
+                            Thêm danh sách
+                        </button>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-6 h-6 cursor-pointer"
+                            onClick={toggleOpenNewColumn}
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
